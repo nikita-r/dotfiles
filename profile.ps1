06e35594d06dcd43576c9d4a888c12078bf5120a
nikita-r <Nikita.Retyunskiy@moodys.com>
2019-10-10 13:51:00 -0400

    add isNumeric() to profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -30,6 +30,15 @@ function dir {
 
 <# misc utility funcs #>
 
+function isNumeric($x) {
+  try {
+    0 + $x | Out-Null
+    return $true
+  } catch {
+    return $false
+  }
+}
+
 function Test-Elevated {
     $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)

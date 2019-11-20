656a86721a565aa5c5db540f11ef6c06e5545389
nikita-r <Nikita.Retyunskiy@moodys.com>
2019-11-20 09:41:00 -0500

    Sublime-Text-config

--- profile.ps1
+++ profile.ps1
@@ -115,7 +115,7 @@ function DeepCopy-Object ($obj) {
 
 function Get-Timestamp {
     $datetime = (Get-Date).ToUniversalTime()
-    (Get-Date $datetime -f s) + (Get-Date $datetime -F:.fffZ)
+    (Get-Date $datetime -f s) + (Get-Date $datetime -F.fffZ)
 }
 
 function Get-LoremIpsum { “Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Nam hendrerit nisi sed sollicitudin pellentesque.  Nunc posuere purus rhoncus pulvinar aliquam.  Ut aliquet tristique nisl vitae volutpat.  Nulla aliquet porttitor venenatis.  Donec a dui et dui fringilla consectetur id nec massa.  Aliquam erat volutpat.  Sed ut dui ut lacus dictum fermentum vel tincidunt neque.  Sed sed lacinia lectus.  Duis sit amet sodales felis.  Duis nunc eros, mattis at dui ac, convallis semper risus.  In adipiscing ultrices tellus, in suscipit massa vehicula eu.” }

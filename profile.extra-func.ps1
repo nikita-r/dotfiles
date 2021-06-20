4957ba0fab07549fa1357ef3acf0ed0df25372d2
nikita-r <nikita-r@PureOS>
2021-06-19 22:45:00 -0400

    some VS Code setup

--- profile.extra-func.ps1
+++ profile.extra-func.ps1
@@ -23,3 +23,77 @@ function DeepCopy-Object ($obj) {
     $fmtBin.Deserialize($memStr)
 }
 
+
+function Reload-Path {
+  $env:Path = [Environment]::GetEnvironmentVariable('Path', 'User') `
+      + ';' + [Environment]::GetEnvironmentVariable('Path', 'Machine')
+}
+
+function ReplaceWith-SymbolicLink ($AbsPath, $target) {
+  if (-not (Test-Path -Path (Split-Path $AbsPath) -Type Container)) {
+    New-Item -ItemType Directory -Path (Split-Path $AbsPath)
+  }
+  if (Test-Path -Path $AbsPath) {
+    Remove-Item -Path $AbsPath
+  }
+  New-Item -ItemType SymbolicLink -Path $AbsPath -Value $target
+}
+
+
+function Install-App ($url, $OutFile, $Arguments) {
+  if (Test-Path -Path "$OutFile.skip") {
+    return
+  }
+# if (Test-Path -Path $OutFile) {
+#   Remove-Item -Path $OutFile
+# }
+  if (-not (Test-Path -Path $OutFile)) {
+    $ProgressPreference, $tmp = "SilentlyContinue", $ProgressPreference
+    Invoke-WebRequest -Uri $url -OutFile $OutFile
+    $ProgressPreference = $tmp
+  }
+  if ($Arguments.Count) {
+    $Process = Start-Process -FilePath $OutFile -Wait -PassThru -ArgumentList $Arguments
+  } else {
+    $Process = Start-Process -FilePath $OutFile -Wait -PassThru
+  }
+  if ($Process.ExitCode) {
+    Write-Error "`"${OutFile}`" installation has failed.  ExitCode=${Process.ExitCode}"
+    return
+  }
+  Set-Content -Path "$OutFile.skip" -Value 'skip'
+}
+
+function Install-App-7z {
+  Install-App https://www.7-zip.org/a/7z1900-x64.exe `
+              -OutFile setup-7z.exe -Arguments @('/S')
+}
+
+function Install-App-Insomnia {
+  Install-App https://updates.insomnia.rest/downloads/windows/latest `
+              -OutFile setup-insomnia.exe -Arguments @('--silent')
+}
+
+function Install-App-VSCode {
+  Install-App https://aka.ms/win32-x64-user-stable `
+              -OutFile setup-vscode.exe -Arguments @(
+                "/SP-"
+                "/SILENT"
+                "/SUPPRESSMSGBOXES"
+                "/TASKS=""addcontextmenufiles,addcontextmenufolders,addtopath"""
+                "/LOG=""setup-vscode.log"""
+              )
+}
+
+function Configure-App-VSCode {
+  ReplaceWith-SymbolicLink "$home\AppData\Roaming\Code\User\keybindings.json" `
+                      -Target "${MyDotfiles}\vscode\keybindings.json"
+# ReplaceWith-SymbolicLink "$home\AppData\Roaming\Code\User\settings.json" `
+#                     -Target "${MyDotfiles}\vscode\settings.json"
+  Copy-Item -LiteralPath "${MyDotfiles}\vscode\settings.json" `
+            -Destination "$home\AppData\Roaming\Code\User\settings.json" `
+            #-Force -Confirm:$false
+  Get-Content "${MyDotfiles}\vscode\extensions.txt" |% { code --install-extension $_ }
+}
+
+

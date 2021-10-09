98c241d748e7ed6407c8f00c870918a05f12fa64
nikita-r <Nikita.Retyunskiy@moodys.com>
2021-10-08 22:50:00 -0400

    chmod=+x for install.sh; Base_bashrc; "dotfiles.repository"

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -f
 set -x
 
 ln -sf "$(pwd)/gitconfig" "${HOME}/.gitconfig"
+
+ln -sf "$(pwd)/Base_bashrc" "${HOME}/.bashrc"
+
+ln -sf -T "$(pwd)/vim" "${HOME}/.vim"
 ln -sf "$(pwd)/vimrc" "${HOME}/.vimrc"
-ln -sf "$(pwd)/vim" "${HOME}/.vim"
 

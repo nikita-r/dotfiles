e406f333405fb3a506f7225a1491f841eb015b22
nikita-r <Nikita.Retyunskiy@moodys.com>
2021-10-15 12:05:00 -0400

    prev+

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ set -x
 ln -sT -f "$cwd/gitconfig" "${HOME}/.gitconfig"
 
   if grep -wiq microsoft /proc/sys/kernel/osrelease; then
-ln -sT -f "$cwd/inputrc" "${HOME}/.inputrc"
     if [ -d ~/.vscode-server ]; then
 ln -sT -f "$cwd/VSC_bashrc" "${HOME}/.bashrc"
     else
+ln -sT -f "$cwd/inputrc" "${HOME}/.inputrc"
 ln -sT -f "$cwd/WSL_bashrc" "${HOME}/.bashrc"
     fi
   else

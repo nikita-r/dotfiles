6dfb84cb0af57617b75edd65008e2adba1bbb16b
nikita-r <Nikita.Retyunskiy@moodys.com>
2021-10-15 10:25:00 -0400

    Add VSC_bashrc

--- install.sh
+++ install.sh
@@ -9,12 +9,16 @@ set -x
 
 ln -sT -f "$cwd/gitconfig" "${HOME}/.gitconfig"
 
-if grep -wiq microsoft /proc/sys/kernel/osrelease; then
+  if grep -wiq microsoft /proc/sys/kernel/osrelease; then
 ln -sT -f "$cwd/inputrc" "${HOME}/.inputrc"
+    if [ -d ~/.vscode-server ]; then
+ln -sT -f "$cwd/VSC_bashrc" "${HOME}/.bashrc"
+    else
 ln -sT -f "$cwd/WSL_bashrc" "${HOME}/.bashrc"
-else
+    fi
+  else
 ln -sT -f "$cwd/Base_bashrc" "${HOME}/.bashrc"
-fi
+  fi
 
 ln -sT -f "$cwd/vim" "${HOME}/.vim"
 ln -sT -f "$cwd/vimrc" "${HOME}/.vimrc"

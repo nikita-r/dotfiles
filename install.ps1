23fc5c61f26e96aba93be7c1ca01bd9ff357283d
nikita-r <Nikita.Retyunskiy@moodys.com>
2021-10-28 14:00:00 -0400

    install.ps1: code --install-extension & +vscode-json

--- install.ps1
+++ install.ps1
@@ -1,7 +1,13 @@
 
 New-Item $env:AppData\Code\User -Type Dir -ea:0 | Out-Null
 
+$urlDotCode = 'https://raw.githubusercontent.com/nikita-r/dotfiles/master/vscode'
+
 'keybindings.json', 'settings.json' |% {
-  iwr https://raw.githubusercontent.com/nikita-r/dotfiles/master/vscode/$_ -OutFile $env:AppData\Code\User\$_
+  iwr $urlDotCode/$_ -OutFile $env:AppData\Code\User\$_
+}
+
+-split((iwr $urlDotCode/extensions.txt).Content) |? { $_ -notLike '#*' } |% {
+  code --install-extension $_
 }
 

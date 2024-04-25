863bc62b6ae9ecc652f3765a7f1ef1cc9b0d2fda
nikita-r <Nikita.Retyunskiy@moodys.com>
2024-04-25 17:25:40 -0400

    Code KB: ^+G to Open Link, ^+O to Open Workspace, file-ext-switcher

--- install.ps1
+++ install.ps1
@@ -52,6 +52,7 @@ New-Item $dirCode\User -Type Dir -ea:0 | Out-Null
 
 $tmp = New-TemporaryFile
 $tmp = Rename-Item $tmp ($tmp.FullName + '.vsix') -PassThru
+'JohannesRudolph.file-ext-switcher-3.2.0',
 'ctf0.macros-1.1.0', 'ryu1kn.text-marker-1.11.0', 'Tyriar.sort-lines-1.11.0', 'melishev.feather-vscode-1.0.1' |% {
   Write-Host "$_.vsix"
   Invoke-WebRequest $urlRaw/vscode-vsix/"$_.vsix" -OutFile $tmp

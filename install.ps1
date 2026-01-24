7938d8f42f0e19689aefceaa2e978081f0aaaa3f
nikita-r <Nikita.Retyunskiy@moodys.com>
2026-01-24 05:00:00 -0500

    Code KB: ⌥ ⌘ [NP]

--- install.ps1
+++ install.ps1
@@ -14,7 +14,7 @@ $json -split "`n" |% {
   if ($_ -match '"ctrl\+shift\+a"') { return }
   if ($_ -match '"command": ""') { return $_ }
   if ($_ -match '"key": "ctrl\+[ij]"') { return $_ }
-  if ($_ -match '"ctrl\+alt\+[anpr]"') { return $_ }
+  if ($_ -match '"ctrl\+alt\+[ar]"') { return $_ }
   if ($_ -match '"command": "-[^"]+"') { return $_ }
   if ($_ -match '"command": "noop"') {
     #if ($_ -match '"ctrl\+shift\+[un]"') { return }
